f5dda812f32f4315b841e32f0644dfaba3ff3dc6
Dhairya <dhairya.ostwal@gmail.com>
2022-09-20 13:36:55 +0530

    ➕ Added more controllers

--- controllers/links.controller.js
+++ controllers/links.controller.js
@@ -9,6 +9,20 @@ const getAllLinks = async (req, res) => {
   }
 };
 
+const getALink = async (req, res) => {
+  try {
+    const { id: linkId } = req.params;
+    const shorturl = await ShortURL.findOne({ _id: linkId });
+    if (!shorturl) {
+      return res.status(404).json({ msg: `No link with id: ${linkId}` });
+    }
+
+    res.status(200).json(shorturl);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
 const createNewLink = async (req, res) => {
   try {
     const shorturl = await ShortURL.create(req.body);
@@ -18,4 +32,45 @@ const createNewLink = async (req, res) => {
   }
 };
 
-module.exports = { getAllLinks, createNewLink };
+const editALink = async (req, res) => {
+  try {
+    const { id: linkId } = req.params;
+    const shorturl = await ShortURL.findOneAndUpdate(
+      { _id: linkId },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!shorturl) {
+      return res.status(404).json({ msg: `No task with id: ${linkId}` });
+    }
+
+    res.status(200).json({ shorturl });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const deleteALink = async (req, res) => {
+  try {
+    const { id: linkId } = req.params;
+    const shorturl = await ShortURL.findOneAndDelete({ _id: linkId });
+    if (!shorturl) {
+      return res.status(404).json({ msg: `No task with id: ${linkId}` });
+    }
+
+    res.status(200).json({ msg: 'Task deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+module.exports = {
+  getAllLinks,
+  createNewLink,
+  getALink,
+  editALink,
+  deleteALink,
+};
--- routes/links.js
+++ routes/links.js
@@ -2,10 +2,13 @@ const express = require('express');
 const {
   getAllLinks,
   createNewLink,
+  getALink,
+  editALink,
+  deleteALink,
 } = require('../controllers/links.controller');
 const router = express.Router();
 
 router.route('/').get(getAllLinks).post(createNewLink);
-// router.route('/:id').get().patch().delete();
+router.route('/:id').get(getALink).patch(editALink).delete(deleteALink);
 
 module.exports = router;
